d41fa74df70b6eba5a0a413c13a600f02c1e94ff
root <root@dvr.downtowncolo.com>
2018-12-04 07:50:38 -0500

    added model

---
+++ js/oga3/modal.js
@@ -0,0 +1,9 @@
+;(function($){
+$( document ).ready(function() {
+  $('.trigger').click(function() {
+     $('.modal-wrapper').toggleClass('open');
+    $('.page-wrapper').toggleClass('blur');
+     return false;
+  });
+});
+})(jQuery);
