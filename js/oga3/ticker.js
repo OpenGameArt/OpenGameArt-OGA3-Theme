e03cff3d756dbb02c44dd25f104f7227529af309
root <root@dvr.downtowncolo.com>
2018-12-04 06:46:18 -0500

    add ticker

---
+++ js/oga3/ticker.js
@@ -0,0 +1,18 @@
+;(function($){
+  $('.marquee').marquee({
+    //speed of the marquee
+    speed: 30,
+    //gap in pixels between the tickers
+    gap: 0,
+    //time in milliseconds before the marquee will start animating
+    delayBeforeStart: 3000,
+    //'left' or 'right'
+    direction: 'left',
+    //true or false - should the marquee be duplicated to show an effect of continues flow
+    duplicated: true,
+
+    pauseOnHover: true,
+    
+    startVisible: true,
+  });
+})(jQuery);
